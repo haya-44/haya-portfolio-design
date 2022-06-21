36d05087fd7a244ef0bd6f8c7e2dff934698c4d5
haya <66757856+haya-44@users.noreply.github.com>
2022-06-21 18:46:44 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+
+
+
+// アニメーション
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+      // if(topWindow > targetPosition - windowHeight + 100)
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
+
+
+// スローアニメーション
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.slow-animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+      // if(topWindow > targetPosition - windowHeight + 100)
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
